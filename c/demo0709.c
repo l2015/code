a145d5ddd23c66121af3217791fcc94ab7f4a974
l2015 <1053778503@qq.com>
2022-11-26 15:39:42 +0800

    second commit

--- c/demo0709.c
+++ c/demo0709.c
@@ -1,22 +1,27 @@
 #include <stdio.h>
 
-int main() {
+int main()
+{
 	int max(int x, int y);
-	int a[10],m,n,i;
+	int a[10], m, n, i;
 	printf("enter 10 integer numbers :");
-	for (i=0; i<10; i++) {
-		scanf("%d",&a[i]);
+	for (i = 0; i < 10; i++)
+	{
+		scanf("%d", &a[i]);
 	}
 	printf("\n");
-	for (i=1,m=a[0],n=0; i<10; i++) {
-		if (max(m, a[i])>m) {
-			m=max(m, a[i]);
-			n=i;
+	for (i = 1, m = a[0], n = 0; i < 10; i++)
+	{
+		if (max(m, a[i]) > m)
+		{
+			m = max(m, a[i]);
+			n = i;
 		}
 	}
-	printf("The largest number is %d\n it is the %dth number.\n",m,n+1);
+	printf("The largest number is %d\n it is the %dth number.\n", m, n + 1);
 }
 
-int max(int x,int y){
-	return (x>y?x:y);
+int max(int x, int y)
+{
+	return (x > y ? x : y);
 }
